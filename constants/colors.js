00274a7cafd8e88547f817572bf045af602c1b74
GretaVait895 <gretavait@gmail.com>
2021-10-28 17:02:53 +0300

    home screen styling

---
+++ constants/colors.js
@@ -0,0 +1,6 @@
+export default {
+  black: '#333333',
+  purple: '#4C416B',
+  grey: '#777777',
+  lightGrey: '#EEEEEE'
+}
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,17 +1,25 @@
 // Base
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
 // Navigation
 import { useNavigation } from '@react-navigation/core'
+import { Icon } from 'react-native-elements'
+import colors from '../constants/colors'
 
 const HomeScreen = () => { 
   const nav = useNavigation()
 
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
-      <TouchableOpacity onPress={() => { nav.navigate('NoteScreen') }}>
-        <Text>Add Note</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>Reminders</Text>
+        <TouchableOpacity style={styles.menu}>
+          <View style={{ ...styles.menuLine, marginBottom: 4 }} />
+          <View style={styles.menuLine} />
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={ styles.addNoteButton } onPress={() => { nav.navigate('NoteScreen') }}>
+        <Icon name="add-outline" type="ionicon" size={48} color="white" />
       </TouchableOpacity>
     </View>
   ) 
@@ -21,8 +29,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingVertical: 32,
-    paddingHorizontal: 16,
+    paddingHorizontal: 8,
     backgroundColor: 'white'
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  addNoteButton: {
+    position: 'absolute',
+    bottom: 8,
+    right: 8,
+    backgroundColor: colors.purple,
+    borderRadius: 500,
+    width: 64,
+    height: 64,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  menu: {
+    backgroundColor: colors.lightGrey,
+    width: 48,
+    height: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 16
+  },
+  menuLine: {
+    width: 20,
+    height: 2,
+    backgroundColor: colors.black,
   }
 })
 
