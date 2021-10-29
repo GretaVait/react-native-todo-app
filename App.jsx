90735f6269c89de467e8f45bfe94e083fa39c4e4
GretaVait895 <gretavait@gmail.com>
2021-10-29 10:52:01 +0300

    safe area view fixed

--- App.jsx
+++ App.jsx
@@ -1,8 +1,8 @@
 // Base
 import React from 'react'
-import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, View, Platform } from 'react-native'
 // Lib
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context'
 // Navigation
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
@@ -19,27 +19,29 @@ export default function App() {
   const Stack = createNativeStackNavigator()
 
   return (
-    <NavigationContainer>
-      <SafeAreaProvider>
-        <KeyboardAvoidingView behavior="height" style={styles.container}>
-          
-          <View style={{ width: 32 }}>
-            <Catgories />
-          </View>
-          
-          <View style={{ flex: 1 }}>
-            <Stack.Navigator>
-              <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
-              <Stack.Screen name="NoteScreen" component={NoteScreen} options={{ headerShown: false }} />
-              <Stack.Screen name="WorkCategoryScreen" component={WorkCategoryScreen} options={{ headerShown: false }} />
-              <Stack.Screen name="IdeasCategoryScreen" component={IdeasCategoryScreen} options={{ headerShown: false }} />
-              <Stack.Screen name="SettingsScreen" component={SettingsScreen} options={{ headerShown: false }} />
-            </Stack.Navigator>
-          </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaView style={styles.container}>
+          <KeyboardAvoidingView behavior="height" style={styles.container}>
+            
+            <View style={{ width: 32 }}>
+              <Catgories />
+            </View>
+            
+            <View style={{ flex: 1 }}>
+              <Stack.Navigator>
+                <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="NoteScreen" component={NoteScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="WorkCategoryScreen" component={WorkCategoryScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="IdeasCategoryScreen" component={IdeasCategoryScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="SettingsScreen" component={SettingsScreen} options={{ headerShown: false }} />
+              </Stack.Navigator>
+            </View>
 
-        </KeyboardAvoidingView>
-      </SafeAreaProvider>
-    </NavigationContainer>
+          </KeyboardAvoidingView>
+        </SafeAreaView>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
@@ -47,7 +49,6 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     flex: 1,
-    paddingTop: 24,
-    backgroundColor: 'white'
+    backgroundColor: Platform.OS === "ios" ? 'black' : 'white'
   },
 });
